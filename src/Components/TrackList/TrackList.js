f7d02340611957db60710e365c0781b2080cf14e
shuknorris <giorgibatoni2110@gmail.com>
2021-06-05 23:11:18 +0400

    gio
    Gio

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import './TrackList.css';
+
+import Track from '../Track/Track';
+
+class TrackList extends React.Component {
+    isInPlaylist(track) {
+        let tracksIn = this.props.tracksInPlaylist;
+        if (!tracksIn) {
+            return;
+        }
+        return (tracksIn.find(savedTrack => (savedTrack.id === track.id))) ? true : false;
+    }
+
+    render() {
+        return (
+            <div className="TrackList">
+                {
+                    this.props.tracks.map(track => {
+                        if (!this.isInPlaylist(track)) {
+                            return <Track key={track.id}
+                                track={track}
+                                onAdd={this.props.onAdd}
+                                onRemove={this.props.onRemove}
+                                isRemoval={this.props.isRemoval}
+                            />
+                        } else {
+                            return null;
+                        }
+
+                    })
+                }
+            </div>
+        );
+    }
+}
+
+export default TrackList;
